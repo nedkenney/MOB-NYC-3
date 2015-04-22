16a8f87dd1592ff76b83295aeb86ed4e55a1ae81
nedkenney <nedkenney@gmail.com>
2015-04-21 21:44:55 -0400

    Added Assignment 0

---
+++ Exercises/Lesson 00/Lame Relationship Facebook Photos/Lame Relationship Facebook Photos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lame Relationship Facebook Photos
+//
+//  Created by Ned Kenney on 4/16/15.
+//  Copyright (c) 2015 Ned Kenney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
